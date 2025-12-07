5506c8caae0cc48bd3d897cfc4044584950580c1
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2025-12-07 18:47:53 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 204 ms, Memory: 52044 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,13 +1,16 @@
+import sys 
+input=sys.stdin.readline
+
 n,m=map(int, input().split())
 name_dic={}
 num_dic={}
 for i in range(1,n+1):
-    name=input()
+    name=input().strip()
     name_dic[name]=i
     num_dic[i]=name
 
 for _ in range(m):
-    question=input()
+    question=input().strip()
     if question.isdigit():
         idx=int(question)               
         print(num_dic[idx])
