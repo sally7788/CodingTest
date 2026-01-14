0cbf2640827c1eca09995d13929006a80745acd9
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-01-14 15:22:20 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,20 +1,18 @@
 -- 코드를 입력하세요
-with half_order as (
-    SELECT flavor, sum(total_order) as total_order
+with Torder as (
+    SELECT flavor, total_order
     from first_half 
-    group by flavor
-),
-
-july_order as (
-    select flavor, sum(total_order) as total_order
+    union all 
+    SELECT flavor, total_order
     from july
-    group by flavor)
+)
+
 
 select flavor
 from (
-    select h.flavor, h.total_order+j.total_order as real_total
-    from half_order h
-    join july_order j on h.flavor=j.flavor
-    order by real_total desc
+    select flavor, sum(total_order)
+    from Torder
+    group by flavor
+    order by 2 desc
     limit 3
 ) as f
