025478de5672c5302a7b55894697f3579ffca785
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-01-23 22:17:50 +0900

    [level 2] Title: 호텔 대실, Time: 31.23 ms, Memory: 9.59 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,31 +1,15 @@
 def solution(book_time):
     answer = 0
+    def to_minute(time):
+        h,m=time.split(':')
+        return int(h)*60+int(m)
     
-    def to_minutes(time):
-        h,m=map(int,time.split(":"))
-        return h*60+m
-    
-#     book_time=sorted([(to_minutes(start), to_minutes(end)+10) for start, end in book_time])
-    
-#     rooms=[]
-#     for start, end in book_time:
-#         for i in range(len(rooms)):
-#             if rooms[i] <= start:
-#                 rooms[i]=end
-#                 break
-#         else: 
-#             rooms.append(end)
-#         rooms.sort()
-                
-#     return len(rooms)
-    time_table=[0 for _ in range(60*24)]
-    
-    for start, end in book_time:
-        start,end=to_minutes(start),to_minutes(end)+10
-        if end > 60*24-1:
-            end=60*24-1
-        for i in range(start,end):
-            time_table[i]+=1
-    return max(time_table)
-            
+    total_time=[0 for _ in range(24*60)]
+    for start,end in book_time:
+        m_start=to_minute(start)
+        m_end=to_minute(end)+10 if to_minute(end)+10 <= 24*60-1 else 24*60-1      
+        for i in range(m_start,m_end):
+            total_time[i]+=1
+    return max(total_time)
+
         
