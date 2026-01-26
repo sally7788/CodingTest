1c3b3d28b5416338169532f37ec05b7562375adb
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-01-27 00:08:21 +0900

    [Silver II] Title: 예산, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,28 +1,22 @@
-import sys
-
-input=sys.stdin.readline
-
 n=int(input())
-req=list(map(int, input().split()))
+req=list(map(int,input().split()))
 m=int(input())
-
-if sum(req) <= m:
-    print(max(req))
-    exit()
 
 left=0
 right=max(req)
 answer=0
-
-while left <= right:
-    mid=(left+right)//2
-    total=0
-    for r in req:
-        total+=min(r,mid)
-    
-    if total <= m:
-        answer=mid
-        left = mid+1
-    elif total > m:
-        right = mid -1 
-print(answer)    
+if sum(req) <= m:
+    print(max(req))
+    exit()
+else:
+    while left <= right:
+        mid=(left+right)//2
+        budget=0
+        for r in req:
+            budget+=min(mid,r)
+        if budget > m:
+            right=mid-1
+        if budget <= m:
+            answer=mid
+            left=mid+1
+print(answer)
