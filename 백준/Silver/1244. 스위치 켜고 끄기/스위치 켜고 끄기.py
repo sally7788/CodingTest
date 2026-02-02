c4362905bc9f0ac693164150823b63a3b74c3d41
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-02-02 23:48:50 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,29 +1,24 @@
-import sys
-input=sys.stdin.readline
-
-s=int(input())
+n=int(input())
 switch=list(map(int, input().split()))
+stu_n=int(input())
 
-st_n=int(input())
-for _ in range(st_n):
-    sex, num = map(int, input().split())
+for _ in range(stu_n):
+    sex, num=map(int, input().split())
     if sex == 1:
-        for i in range(s):
-            if (i+1) % num == 0:                
+        for i in range(n):
+            if (i+1) % num == 0:
                 switch[i]=1-switch[i]
-                
-    elif sex == 2:
-        left=num-1
-        right=num-1
-
-        while left-1 >= 0 and right+1<s and switch[left-1] == switch[right+1]:
+    else: 
+        left, right=num-1, num-1
+        
+        while left-1>=0 and right+1<n and switch[left-1] == switch[right+1]:
             left-=1
             right+=1
-        
+            
         for i in range(left, right+1):
             switch[i]=1-switch[i]
 
-for i in range(s):
+for i in range(n):
     print(switch[i], end=" ")
-    if (i+1)%20==0:
-        print()     
+    if (i+1) % 20 == 0:
+        print()
