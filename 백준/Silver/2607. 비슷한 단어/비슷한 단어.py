6eb30bf3a0169b35361729eaa97d044d710a428d
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-02-24 18:27:25 +0900

    [Silver II] Title: 비슷한 단어, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,24 +1,19 @@
-import sys
-
-input=sys.stdin.readline
 n=int(input())
-
-first=input().strip() 
-first_dic={chr(i):0 for i in range(ord('A'), ord('Z')+1)}
-for f in first:    
-    first_dic[f]=first_dic.get(f,0)+1
-
-answer=0
+first=input()
+ans=0
 for _ in range(n-1):
-    word=input().strip()
-    word_dic={chr(i):0 for i in range(ord('A'), ord('Z')+1)}
-    for w in word:
-        word_dic[w]=word_dic.get(w,0)+1
+    word=input()
 
     cnt=0
-    for i in range(ord('A'), ord('Z')+1):
-        cnt+=abs(first_dic[chr(i)]-word_dic[chr(i)])
-            
+    first_count=[0]*26
+    word_count=[0]*26
+
+    for c in first:
+        first_count[ord(c)-65]+=1
+    for c in word:
+        word_count[ord(c)-65]+=1
+    for i in range(26):
+        cnt+= abs(first_count[i]-word_count[i])
     if cnt <= 2 and abs(len(first)-len(word)) <=1:
-        answer+=1
-print(answer)
+        ans+=1
+print(ans)
