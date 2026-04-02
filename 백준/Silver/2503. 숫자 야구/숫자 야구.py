f0ba9ccca2e35b6f38abd707b342e1b4f642f8ae
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-04-02 22:41:13 +0900

    [Silver III] Title: 숫자 야구, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,27 +1,33 @@
-def check(candidate, condition):
-    strike, ball =0,0
+
+def check(candidate, nums):
+    strike, ball=0,0
     for i in range(3):
-        if candidate[i]==condition[i]:
+        if candidate[i] == nums[i]:
             strike+=1
-        elif candidate[i] in condition:
-            ball+=1
+        else: 
+            if nums[i] in candidate:
+                ball+=1
     return strike, ball 
 
 n=int(input())
-questions=[tuple(map(int,input().split())) for _ in range(n)]
+data = [input().split() for _ in range(n)]
 answer=0
-for a in range(1,10):
-    for b in range(1,10):
-        for c in range(1,10):
-            if a==b or a==c or b==c:
-                continue
-            candidate=str(a)+str(b)+str(c)
-            ok=True
-            for num, st, ba in questions:
-                strike,ball = check(candidate, str(num))
-                if strike != st or ball != ba:
-                    ok=False
-                    break 
-            if ok:
-                answer+=1
+  
+
+for i in range(123,1000):
+    str_i=str(i)
+    if '0' in str_i or len(set(str_i)) != 3:
+        continue
+    valid=True
+
+    for nums, strike, ball in data:
+        strike=int(strike)
+        ball=int(ball)
+
+        s, b = check(str_i, nums)
+        if strike != s or ball != b:
+            valid=False
+            break 
+    if valid==True:
+        answer+=1
 print(answer)
