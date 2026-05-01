0ffe8a5a616fc3cc540e23353d2ad3269ef89566
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-05-01 19:47:22 +0900

    [level 2] Title: 구명보트, Time: 8.87 ms, Memory: 9.69 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    end=len(people)-1
+    start=0
+    while start <= end: 
+        
+        if people[start] + people[end] <= limit :
+            start+=1
+            end-=1
+        else: 
+            end-=1
+        answer+=1
+        
+    return answer
