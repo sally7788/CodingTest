ec50e0a798f9f69636f9cf9a6d388b8b46bd088d
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-07-20 23:48:28 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,14 +1,15 @@
 def solution(d, budget):
     answer = 0
-    d.sort()
     
-    for i in d:
+    d.sort()
+    curr=0
+    
+    for i in d: 
         budget-=i
         
         if budget < 0:
             break 
         answer+=1
-            
-    
+        
         
     return answer
