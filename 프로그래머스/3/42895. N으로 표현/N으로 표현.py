e7d80f38e503d3da2f8a2de0d161ce79b1e149b6
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-07-22 21:41:11 +0900

    [level 3] Title: N으로 표현, Time: 9.03 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,19 +1,21 @@
 def solution(N, number):
-    dp=[set() for i in range(9)] #n을 쓰는 횟수에 따라서 dp에 저장 
+    answer = 0
+    dp=[set() for i in range(9)]
+    
     for i in range(1,9):
         dp[i].add(int(str(N)*i))
         for j in range(1,i):
             for first in dp[j]:
-                for second in dp[i-j]: 
-                    dp[i].add(first + second)
+                for second in dp[i-j]:
+                    dp[i].add(first+second)
                     dp[i].add(first*second)
-                    dp[i].add(first - second)
+                    dp[i].add(first-second)
                     dp[i].add(second-first)
-                    if first != 0:
+                    if first != 0: 
                         dp[i].add(second//first)
-                    if second != 0:
+                    if second != 0: 
                         dp[i].add(first//second)
         if number in dp[i]:
             return i
-                        
+            
     return -1
