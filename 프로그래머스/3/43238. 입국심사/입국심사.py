acb1d5bb937c3aab0d5d3ea0eb2240cf981d366b
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-07-22 21:19:14 +0900

    [level 3] Title: 입국심사, Time: 344.23 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,18 @@
 def solution(n, times):
-    left = 0
-    right = max(times)*n
-        
     
-    while right >= left:
+    left=0
+    right=max(times)*n
+    
+    while left <= right: 
         total=0
-        mid=int((left+right))//2 #시간 
+        mid=int((left+right)//2) #시간 
         
         for t in times: 
-            total += mid//t
-        if total >= n:
+            total +=mid//t
+        if total >= n: 
             right=mid-1
         else: 
             left=mid+1
-            
+        
+        
     return left
