c568626bfa71a304573b2cdfdbc5fa2d9f8e4b1f
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-07-22 21:09:51 +0900

    [level 2] Title: 점 찍기, Time: 164.46 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,10 +1,9 @@
 def solution(k, d):
     answer = 0
-    
-    for i in range(d//k+1):
-        x=i*k
+    for i in range(0, d//k+1):
+        x=i*k 
         max_y=(d**2-x**2)**0.5
-        max_j=max_y//k
+        max_j=max_y//k #y가 몇번 들어갔는지 y=j*k의 j의 최댓값 
         answer+=(max_j+1)
-
+        
     return answer
