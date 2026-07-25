253834800ef6df1193e744afd627e860fceb033b
Kang yunjoo <143007050+sally7788@users.noreply.github.com>
2026-07-25 16:22:00 +0900

    [level 2] Title: 광물 캐기, Time: 1.16 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,46 +1,35 @@
 def solution(picks, minerals):
-    '''피로도가 적은 곡갱이를 어디서부터 쓸 것인가? 고민 '''
-    answer=float('inf')
+    answer = float('inf')
     n=len(minerals)
-    piro = [
-        [1, 1, 1],     # 다이아 곡괭이
-        [5, 1, 1],     # 철 곡괭이
-        [25, 5, 1]     # 돌 곡괭이
-    ]
-    
-    def mineral_idx(mineral):
-        if mineral == "diamond":
+    def minerals_change(mineral):
+        if mineral=="diamond":
             return 0
-        elif mineral == "iron":
+        if mineral=="iron":
             return 1
-        return 2
+        else: return 2 
     
+    piro=[[1,1,1],[5,1,1],[25,5,1]]
+    cost=0
     def dfs(idx, picks, cost):
-        nonlocal answer
+        nonlocal answer 
         
         if idx >= n or sum(picks)==0:
-            answer = min(answer, cost)
-            return answer
+            answer=min(answer, cost)
+            return answer 
         
         if cost >= answer: 
             return 
         
-        for pick in range(3): #곡갱이 선택
-            if picks[pick] == 0: 
+        for i in range(3):
+            if picks[i] ==0 : 
                 continue
-            picks[pick]-=1 #곡갱이 골랐다 
+            picks[i]-=1
             next_cost=cost
             
-            for i in range(idx, min(idx+5, n)): #광물 캐기
-                next_cost += piro[pick][mineral_idx(minerals[i])]
-            dfs(idx+5,picks,next_cost)
+            for j in range(idx, min(idx+5, n)):
+                next_cost+=piro[i][minerals_change(minerals[j])]
+            dfs(idx+5, picks, next_cost)
             
-            picks[pick]+=1 #원상복구 하는거임... 
-    dfs(0,picks[:],0)
-            
-    
-        
-            
-        
-        
+            picks[i]+=1
+    dfs(0, picks[:],0)
     return answer
